da3227ca6209927af9f642d49fbe6cde5992714c
Ville Antila <ville.antila@stud.pointcollege.fi>
2018-10-29 19:16:32 +0200

    Tietokannan voi tehdä omalle koneelle tiedoston TyoajanlaskentaEntities.edmx.sql avulla

--- TyoajanlaskentaSovellus/TyoajanlaskentaEntities.edmx.sql
+++ TyoajanlaskentaSovellus/TyoajanlaskentaEntities.edmx.sql
@@ -2,8 +2,8 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 10/12/2018 19:55:00
--- Generated from EDMX file: C:\Users\ville\Source\Repos\Point-PS\TyoajanlaskentaSovellus\TyoajanlaskentaSovellus\TyoajanlaskentaEntities.edmx
+-- Date Created: 10/29/2018 19:08:06
+-- Generated from EDMX file: C:\Users\ville\source\repos\TyoajanlaskentaSovellus\TyoajanlaskentaSovellus\TyoajanlaskentaEntities.edmx
 -- --------------------------------------------------
 
 SET QUOTED_IDENTIFIER OFF;
@@ -17,6 +17,12 @@ GO
 -- Dropping existing FOREIGN KEY constraints
 -- --------------------------------------------------
 
+IF OBJECT_ID(N'[dbo].[Fk_Tunnit_Henkilot]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Tunnit] DROP CONSTRAINT [Fk_Tunnit_Henkilot];
+GO
+IF OBJECT_ID(N'[dbo].[Fk_Tunnit_Tyot]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Tunnit] DROP CONSTRAINT [Fk_Tunnit_Tyot];
+GO
 
 -- --------------------------------------------------
 -- Dropping existing tables
@@ -31,9 +37,6 @@ GO
 IF OBJECT_ID(N'[dbo].[Tyot]', 'U') IS NOT NULL
     DROP TABLE [dbo].[Tyot];
 GO
-IF OBJECT_ID(N'[scrumDatabaseModelStoreContainer].[database_firewall_rules]', 'U') IS NOT NULL
-    DROP TABLE [scrumDatabaseModelStoreContainer].[database_firewall_rules];
-GO
 
 -- --------------------------------------------------
 -- Creating all tables
@@ -41,7 +44,9 @@ GO
 
 -- Creating table 'Henkilot'
 CREATE TABLE [dbo].[Henkilot] (
-    [HenkiloId] int IDENTITY(1,1) NOT NULL
+    [HenkiloId] int IDENTITY(1,1) NOT NULL,
+    [Etunimi] varchar(50)  NULL,
+    [Sukunimi] varchar(50)  NULL
 );
 GO
 
@@ -49,24 +54,16 @@ GO
 CREATE TABLE [dbo].[Tunnit] (
     [TuntiId] int IDENTITY(1,1) NOT NULL,
     [HenkiloId] int  NOT NULL,
-    [TyoId] int  NOT NULL
+    [TyoId] int  NOT NULL,
+    [Tuntimaara] time  NULL
 );
 GO
 
 -- Creating table 'Tyot'
 CREATE TABLE [dbo].[Tyot] (
-    [TyoId] int IDENTITY(1,1) NOT NULL
-);
-GO
-
--- Creating table 'database_firewall_rules'
-CREATE TABLE [dbo].[database_firewall_rules] (
-    [id] int IDENTITY(1,1) NOT NULL,
-    [name] nvarchar(128)  NOT NULL,
-    [start_ip_address] varchar(45)  NOT NULL,
-    [end_ip_address] varchar(45)  NOT NULL,
-    [create_date] datetime  NOT NULL,
-    [modify_date] datetime  NOT NULL
+    [TyoId] int IDENTITY(1,1) NOT NULL,
+    [Tyotunniste] varchar(10)  NULL,
+    [Kuvaus] varchar(255)  NULL
 );
 GO
 
@@ -92,15 +89,39 @@ ADD CONSTRAINT [PK_Tyot]
     PRIMARY KEY CLUSTERED ([TyoId] ASC);
 GO
 
--- Creating primary key on [id], [name], [start_ip_address], [end_ip_address], [create_date], [modify_date] in table 'database_firewall_rules'
-ALTER TABLE [dbo].[database_firewall_rules]
-ADD CONSTRAINT [PK_database_firewall_rules]
-    PRIMARY KEY CLUSTERED ([id], [name], [start_ip_address], [end_ip_address], [create_date], [modify_date] ASC);
-GO
-
 -- --------------------------------------------------
 -- Creating all FOREIGN KEY constraints
 -- --------------------------------------------------
+
+-- Creating foreign key on [HenkiloId] in table 'Tunnit'
+ALTER TABLE [dbo].[Tunnit]
+ADD CONSTRAINT [Fk_Tunnit_Henkilot]
+    FOREIGN KEY ([HenkiloId])
+    REFERENCES [dbo].[Henkilot]
+        ([HenkiloId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'Fk_Tunnit_Henkilot'
+CREATE INDEX [IX_Fk_Tunnit_Henkilot]
+ON [dbo].[Tunnit]
+    ([HenkiloId]);
+GO
+
+-- Creating foreign key on [TyoId] in table 'Tunnit'
+ALTER TABLE [dbo].[Tunnit]
+ADD CONSTRAINT [Fk_Tunnit_Tyot]
+    FOREIGN KEY ([TyoId])
+    REFERENCES [dbo].[Tyot]
+        ([TyoId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'Fk_Tunnit_Tyot'
+CREATE INDEX [IX_Fk_Tunnit_Tyot]
+ON [dbo].[Tunnit]
+    ([TyoId]);
+GO
 
 -- --------------------------------------------------
 -- Script has ended
