a9280203e74a4117e87a1ccc9af5b6ff08e011b0
Bo Furustam <bo.furustam@stud.pointcollege.fi>
2018-11-06 18:21:15 +0200

    Työt -taulu lisäys, muokkaus ja poisto toimii

--- TyoajanlaskentaSovellus/Controllers/TyotController.cs
+++ TyoajanlaskentaSovellus/Controllers/TyotController.cs
@@ -10,9 +10,53 @@ using TyoajanlaskentaSovellus.Models;
 namespace TyoajanlaskentaSovellus.Controllers
 {
     public class TyotController : Controller
-    { 
+    {
+       
+    public ActionResult Update(Tyot tyot)
+        {
+            scrumDatabaseEntities entities = new scrumDatabaseEntities();
 
-         public JsonResult GetSingleTyo(int id)
+            bool OK = false;
+
+            // onko kyseessä muokkaus vai uuden lisääminen?
+            if (tyot.TyoId == 0)
+            {
+                // kyseessä on uuden asiakkaan lisääminen, kopioidaan kentät
+                Tyot dbItem = new Tyot()
+                {
+
+                    Tyotunniste = tyot.Tyotunniste,
+                    Kuvaus = tyot.Kuvaus,
+
+
+                };
+
+                // tallennus tietokantaan
+                entities.Tyot.Add(dbItem);
+                entities.SaveChanges();
+                OK = true;
+            }
+            else
+            {
+                // muokkaus, haetaan id:n perusteella riviä tietokannasta
+                Tyot dbItem = (from t in entities.Tyot
+                               where t.TyoId == tyot.TyoId
+                               select t).FirstOrDefault();
+                if (dbItem != null)
+                {
+                    dbItem.Tyotunniste = tyot.Tyotunniste;
+                    dbItem.Kuvaus = tyot.Kuvaus;
+
+                    // tallennus tietokantaan
+                    entities.SaveChanges();
+                    OK = true;
+                }
+            }
+            entities.Dispose();
+
+            return Json(OK, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetSingleTyo(int id)
     {
 
         {
@@ -38,7 +82,26 @@ namespace TyoajanlaskentaSovellus.Controllers
         {
             return View();
         }
-        
+        public ActionResult Delete(int id)
+        {
+            scrumDatabaseEntities entities = new scrumDatabaseEntities();
+
+            // etsitään id:n perusteella asiakasrivi kannasta
+            bool ok = false;
+            Tyot dbItem = (from t in entities.Tyot
+                           where t.TyoId == id
+                           select t).FirstOrDefault();
+            if (dbItem != null)
+            {
+                // tietokannasta poisto
+                entities.Tyot.Remove(dbItem);
+                entities.SaveChanges();
+                ok = true;
+            }
+            entities.Dispose();
+
+            return Json(ok, JsonRequestBehavior.AllowGet);
+        }
         public JsonResult HaeKaikkiTyot()
         {
             scrumDatabaseEntities entities = new scrumDatabaseEntities();
@@ -59,6 +122,8 @@ namespace TyoajanlaskentaSovellus.Controllers
     }
 }
 
+
+    
 
 
 
