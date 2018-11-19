b5562ddb6330f7c296d9fc8ce6957ce024c76aca
Emma Vantaa <emma.vantaa@gmail.com>
2018-11-19 19:19:05 +0200

    UI ja tyylejä

--- TyoajanlaskentaSovellus/TyoajanlaskentaEntities.Designer.cs
+++ TyoajanlaskentaSovellus/TyoajanlaskentaEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\sundv\source\repos\Point-PS\TyoajanlaskentaSovellus\TyoajanlaskentaSovellus\TyoajanlaskentaEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\catwi\source\repos\TyoajanlaskentaSovellus\TyoajanlaskentaSovellus\TyoajanlaskentaEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
